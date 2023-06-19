c17b8a287efbd7a456367430dff305c367c390ff
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-06-19 19:01:40 -0400

    interface terminer

---
+++ theme.js
@@ -0,0 +1,13 @@
+//selection des elements neccessaire.
+let btn = document.querySelector("#mode");
+txtBtn = document.querySelector("span");
+
+//activation des buttons
+btn.addEventListener('click', () =>{
+    if(document.body.classList.contains('dark')){
+        //Mode claire
+
+    }else{
+        //Mode sombre
+    }
+});
