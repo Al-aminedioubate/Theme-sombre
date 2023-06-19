d9009657702d3b162e93c7c22b26ed5a1a3ed862
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-06-19 19:23:13 -0400

    projet terminer

--- theme.js
+++ theme.js
@@ -2,6 +2,13 @@
 let btn = document.querySelector("#mode");
 let span = document.querySelector("span");
 
+//verification de la preferance de l'utilisateur
+if(localStorage.getItem('theme')){
+    if(localStorage.getItem('theme') == 'sombre'){
+        modeSombre();
+    }
+}
+
 //activation des buttons
 btn.addEventListener('click', () =>{
     if(document.body.classList.contains('dark')){
